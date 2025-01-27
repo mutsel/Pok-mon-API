4be3f9967f14c7dc95eba8f67391320b12577ee4
Anne Vollmann <vollmann.anne@web.de>
2025-01-27 11:40:02 +0100

    pkm_information

--- templates.js
+++ templates.js
@@ -14,3 +14,86 @@ function getPkmCardTemplate() {
 function getPkmTypesTemplate(indexPkmType) {
     return `<div class="${initPkms.types[indexPkmType].type.name}">${initPkms.types[indexPkmType].type.name}</div>`
 }
+
+
+function getSectionAboutTemplate() {
+    return `<section class="section_about">
+                <div class="info_category">
+                    <div>Base Experience</div>
+                    <div>XXX</div>
+                </div>
+                <div class="info_category">
+                    <div>Height</div>
+                    <div>XXX</div>
+                </div>
+                <div class="info_category">
+                    <div>Weight</div>
+                    <div>XXX</div>
+                </div>
+                <div class="info_category">
+                    <div>Abilities</div>
+                    <div>XXX</div>
+                </div>
+                <div class="info_category">
+                    <div>Types</div>
+                    <div class="types_icons">
+                        <img class="XXX" src="./assets/icons/XXX.svg">
+                        <img class="XXX" src="./assets/icons/XXX.svg">
+                    </div>
+                </div>
+            </section>`
+}
+
+
+function getSectionStatsTemplate() {
+    return `<section class="section_stats">
+                <div class="info_category">
+                    <div>HP</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="info_category">
+                    <div>Attack</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="info_category">
+                    <div>Defense</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="info_category">
+                    <div>Sp. Atk</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="info_category">
+                    <div>Sp. Def</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="info_category">
+                    <div>Speed</div>
+                    <div class="stat_value display_flex_centered">
+                        45 <div class="progress_bar">
+                            <div class="grass"></div>
+                            </div>
+                    </div>
+                </div>
+            </section>`
+}
