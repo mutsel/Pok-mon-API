10daf07bd49efa8101d263df45fb34b4d0e9a96d
Anne Vollmann <vollmann.anne@web.de>
2025-01-27 15:24:36 +0100

    render_current_pkm

--- script.js
+++ script.js
@@ -22,36 +22,101 @@ async function loadinitPkmks(){
     for (let indexinitPkms = 0; indexinitPkms < 20; indexinitPkms++) {
         let PKM_URL = initPkmsUrls.results[indexinitPkms].url;
         let pkmDataApi = await fetch(PKM_URL);
-        initPkms = await pkmDataApi.json();
+        let initPkmsEntrie = await pkmDataApi.json();
+        initPkms.push (initPkmsEntrie);
         //console.log(initPkms);
-        document.getElementById("pokedex").innerHTML += getPkmCardTemplate();
+        document.getElementById("pokedex").innerHTML += getPkmCardTemplate(indexinitPkms);
         loadPkmsTypes(indexinitPkms);
     } 
 }
 
 
-function PkmIdThreeDigits() {
-    let IdPkm = initPkms.id.toString();
-    let IdPkmFixed = IdPkm.padStart(3, '0');
-    return `${IdPkmFixed}`;
+function PkmIdThreeDigits(PkmId) {
+    let PkmIdString = PkmId.toString();
+    let PkmIdFixed = PkmIdString.padStart(3, '0');
+    return `${PkmIdFixed}`;
 }
 
 
-async function loadPkmsTypes() {
-    for (let indexPkmType = 0; indexPkmType < initPkms.types.length; indexPkmType++) {
-        document.getElementById("pkm_card_types_"+ initPkms.id).innerHTML += getPkmTypesTemplate(indexPkmType);
+async function loadPkmsTypes(indexinitPkms) {
+    for (let indexPkmType = 0; indexPkmType < initPkms[indexinitPkms].types.length; indexPkmType++) {
+        document.getElementById("pkm_card_types_"+ initPkms[indexinitPkms].id).innerHTML += getPkmTypesNameTemplate(indexinitPkms, indexPkmType);
     }
-    document.getElementById("pkm_" + initPkms.id).style.backgroundImage = "url('./assets/icons/" + initPkms.types[0].type.name + ".svg')";
+
+    let firstType = initPkms[indexinitPkms].types[0].type.name;
+    document.getElementById("pkm_" + initPkms[indexinitPkms].id).style.backgroundImage = "url('./assets/icons/" + firstType + ".svg')";
 }
 
 
-function toggleOverlayPkmCard(pkmId) {
+function renderCurrentPkmCard(indexinitPkms) {
+    let contentRef= document.getElementById("currentPkmCard");
+    contentRef.innerHTML = ""; 
+    contentRef.innerHTML += getCurrentPkmCardTemplate(indexinitPkms);
+    loadContentCurrentPkmCard(indexinitPkms);
     toggleOverlayPkmCard();
-    getCurrentPkmTemplate(pkmId);
+}
+
+
+function loadContentCurrentPkmCard(indexinitPkms) {
+    changeBgCurrentPkmCard(indexinitPkms);
+    PkmId = indexinitPkms +1;
+    document.getElementById("currentPkmId").innerHTML = "#" + PkmIdThreeDigits(PkmId);
+    document.getElementById("currentPkmName").innerHTML = initPkms[indexinitPkms].name.charAt(0).toUpperCase() + initPkms[indexinitPkms].name.slice(1);
+    document.getElementById("currentPkmImg").src = initPkms[indexinitPkms].sprites.other.dream_world.front_default;
+    loadCurrentPkmInfoCategory("", indexinitPkms);
+}
+
+
+function changeBgCurrentPkmCard(indexinitPkms) {
+    let firstType = initPkms[indexinitPkms].types[0].type.name;
+    let cssClassFirstType = document.querySelector("."+firstType);
+    let currentBgColor = getComputedStyle(cssClassFirstType).borderColor;
+    document.getElementById("currentPkmCard").style.backgroundColor = currentBgColor;
 }
 
 
 function toggleOverlayPkmCard() {
     document.getElementById("overlay").classList.toggle("d_none");
     document.getElementById("currentPkmCard").classList.toggle("d_none");
+}
+
+
+function loadCurrentPkmInfoCategory(clickedBtn, indexinitPkms) {
+    //console.log(initPkms[indexinitPkms]);
+    let contentRef= document.getElementById("currentPkmInfo");
+    contentRef.innerHTML = ""; 
+    let currentCategory = clickedBtn;
+    switch (currentCategory) {
+        default:
+        case 'About':
+            contentRef.innerHTML += getSectionAboutTemplate(indexinitPkms);
+            loadCurrentTypes(indexinitPkms);
+            loadCurrentAbilities(indexinitPkms);
+            break;
+        case 'Stats':
+            contentRef.innerHTML += getSectionStatsTemplate(indexinitPkms);
+            break;
+        case 'Evolution':
+            console.log(currentCategory);
+            break;
+        case 'Pokédex':
+            console.log(currentCategory);
+            break;
+    }
+}
+
+
+function loadCurrentTypes(indexinitPkms) {
+    for (let indexPkmType = 0; indexPkmType < initPkms[indexinitPkms].types.length; indexPkmType++) {
+        document.getElementById("typesIcons").innerHTML += getPkmTypesImg(indexinitPkms, indexPkmType);
+    }
+}
+
+
+function loadCurrentAbilities(indexinitPkms) {
+    let contentRef =  document.getElementById("currentAbilities");
+    for (let indexPkmAbility = 0; indexPkmAbility < initPkms[indexinitPkms].abilities.length; indexPkmAbility++) {
+        contentRef.innerHTML += initPkms[indexinitPkms].abilities[indexPkmAbility].ability.name + ", ";
+    }
+    contentRef.innerHTML = contentRef.innerHTML.slice(0, -2);
 }
--- templates.js
+++ templates.js
@@ -1,97 +1,124 @@
-function getPkmCardTemplate() {
+function getPkmCardTemplate(indexinitPkms) {
     return `<div class="pkm_card">
-            <div onclick="toggleOverlayPkmCard(${initPkms.id})" id="pkm_${initPkms.id}" class="pkm_card_content">
-                <img src="${initPkms.sprites.other.dream_world.front_default}" class="pkm_card_img position_top">
-                <span class="pkm_card_id position_top">#${PkmIdThreeDigits()}</span>
-                <span class="pkm_card_name position_top">${initPkms.name.charAt(0).toUpperCase() + initPkms.name.slice(1)}</span>
-                <div id="pkm_card_types_${initPkms.id}"class="pkm_card_types position_top"></div>
+            <div onclick="renderCurrentPkmCard(${indexinitPkms})" id="pkm_${initPkms[indexinitPkms].id}" class="pkm_card_content">
+                <img src="${initPkms[indexinitPkms].sprites.other.dream_world.front_default}" class="pkm_card_img position_top">
+                <span class="pkm_card_id position_top">#${PkmIdThreeDigits(initPkms[indexinitPkms].id)}</span>
+                <span class="pkm_card_name position_top">${initPkms[indexinitPkms].name.charAt(0).toUpperCase() + initPkms[indexinitPkms].name.slice(1)}</span>
+                <div id="pkm_card_types_${initPkms[indexinitPkms].id}"class="pkm_card_types position_top"></div>
             </div>
         </div>
     `
 }
 
 
-function getPkmTypesTemplate(indexPkmType) {
-    return `<div class="${initPkms.types[indexPkmType].type.name}">${initPkms.types[indexPkmType].type.name}</div>`
+function getPkmTypesNameTemplate(indexinitPkms, indexPkmType) {
+    return `<div class="${initPkms[indexinitPkms].types[indexPkmType].type.name}">${initPkms[indexinitPkms].types[indexPkmType].type.name}</div>`
 }
 
 
-function getSectionAboutTemplate() {
+function getCurrentPkmCardTemplate(indexinitPkms) {
+    return `<div id="currentPkmCardOverview">
+                <div id="currentPkmTitle">
+                    <span id="currentPkmId" class="display_flex_centered">#000</span>
+                    <h2 id="currentPkmName"></h2>
+                    <button onclick="toggleOverlayPkmCard()" id="closeCurrentPkm" class="display_flex_centered"><img src="./assets/icons/close_btn.svg"></button>
+                </div>
+                <div id="currentPkmFrame">
+                    <img id="currentPkmImg" src="">
+                </div>
+            </div>
+            <div id="currentPkmCardContent">
+                <div id="" class="current_pkm_navbar">
+                    <button onclick="loadCurrentPkmInfoCategory('About', ${indexinitPkms})">About</button>
+                    <button onclick="loadCurrentPkmInfoCategory('Stats', ${indexinitPkms})">Stats</button>
+                    <button onclick="loadCurrentPkmInfoCategory('Evolution', ${indexinitPkms})">Evolution</button>
+                    <button onclick="loadCurrentPkmInfoCategory('Pokédex', ${indexinitPkms})">Pokédex</button>
+                </div>
+                <div id="currentPkmInfo">
+                    
+                </div>
+            </div>`
+}
+
+
+function getSectionAboutTemplate(indexinitPkms) {
     return `<section class="section_about">
                 <div class="info_category">
                     <div>Base Experience</div>
-                    <div>XXX</div>
+                    <div>${initPkms[indexinitPkms].base_experience}</div>
                 </div>
                 <div class="info_category">
                     <div>Height</div>
-                    <div>XXX</div>
+                    <div>${initPkms[indexinitPkms].height}</div>
                 </div>
                 <div class="info_category">
                     <div>Weight</div>
-                    <div>XXX</div>
+                    <div>${initPkms[indexinitPkms].weight}</div>
                 </div>
                 <div class="info_category">
                     <div>Abilities</div>
-                    <div>XXX</div>
+                    <div id="currentAbilities"></div>
                 </div>
                 <div class="info_category">
                     <div>Types</div>
-                    <div class="types_icons">
-                        <img class="XXX" src="./assets/icons/XXX.svg">
-                        <img class="XXX" src="./assets/icons/XXX.svg">
-                    </div>
+                    <div id="typesIcons"></div>
                 </div>
             </section>`
 }
 
 
-function getSectionStatsTemplate() {
+function getPkmTypesImg(indexinitPkms, indexPkmType) {
+    return `<img class="${initPkms[indexinitPkms].types[indexPkmType].type.name}" src="./assets/icons/${initPkms[indexinitPkms].types[indexPkmType].type.name}.svg">`
+}
+
+
+function getSectionStatsTemplate(indexinitPkms) {
     return `<section class="section_stats">
                 <div class="info_category">
                     <div>HP</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[0].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[0].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
                 <div class="info_category">
                     <div>Attack</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[1].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[1].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
                 <div class="info_category">
                     <div>Defense</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[2].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[2].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
                 <div class="info_category">
                     <div>Sp. Atk</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[3].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[3].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
                 <div class="info_category">
                     <div>Sp. Def</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[4].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[4].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
                 <div class="info_category">
                     <div>Speed</div>
                     <div class="stat_value display_flex_centered">
-                        45 <div class="progress_bar">
-                            <div class="grass"></div>
+                        ${initPkms[indexinitPkms].stats[5].base_stat} <div class="progress_bar">
+                            <div style="width: ${initPkms[indexinitPkms].stats[5].base_stat}px" class="${initPkms[indexinitPkms].types[0].type.name}"></div>
                             </div>
                     </div>
                 </div>
